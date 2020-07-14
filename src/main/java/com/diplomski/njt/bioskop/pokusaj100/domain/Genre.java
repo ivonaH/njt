f9662a675ff79c31d8a65b1c73826f8087637684
ivona <>
2020-07-14 08:17:06 +0200

    Dodate klase Genre i Movie.

---
+++ src/main/java/com/diplomski/njt/bioskop/pokusaj100/domain/Genre.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.diplomski.njt.bioskop.pokusaj100.domain;
+
+/**
+ *
+ * @author root
+ */
+public enum Genre {
+    action, drama, romance, mistery, horror
+}
---
+++ src/main/java/com/diplomski/njt/bioskop/pokusaj100/domain/Movie.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.diplomski.njt.bioskop.pokusaj100.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author root
+ */
+@Entity
+public class Movie {
+
+    @Id
+    private int id;
+    private String name;
+    private String director;
+    private Genre genre;
+    @ManyToOne
+    @JoinColumn(name="userId")
+    private User user;
+    private int year;
+
+    public Movie() {
+    }
+
+    public Movie(int id, String name, String director, Genre genre, User user, int year) {
+        this.id = id;
+        this.name = name;
+        this.director = director;
+        this.genre = genre;
+        this.user = user;
+        this.year = year;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + year + " " + genre;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+}
