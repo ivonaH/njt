82409fc3ac63b0d78f345e2275e52725cd678ac3
ivona <root@LAPTOP-HCPCR4F4>
2020-08-04 23:07:54 +0200

    Dodata validacija za email, ime i prezime rezervacije i da li ima mesta na projekciji.

--- src/main/java/com/diplomski/njt/bioskop/pokusaj100/validator/ReservationValidator.java
+++ src/main/java/com/diplomski/njt/bioskop/pokusaj100/validator/ReservationValidator.java
@@ -6,7 +6,9 @@
 package com.diplomski.njt.bioskop.pokusaj100.validator;
 
 import com.diplomski.njt.bioskop.pokusaj100.domain.Reservation;
+import com.diplomski.njt.bioskop.pokusaj100.service.ReservationService;
 import java.util.Calendar;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
@@ -19,6 +21,13 @@ import org.springframework.validation.Validator;
 @Component
 public class ReservationValidator implements Validator {
 
+    private final ReservationService reservationService;
+
+    @Autowired
+    public ReservationValidator(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
     @Override
     public boolean supports(Class<?> type) {
         return Reservation.class.equals(type);
@@ -26,12 +35,52 @@ public class ReservationValidator implements Validator {
 
     @Override
     public void validate(Object o, Errors errors) {
+        Reservation reservation = (Reservation) o;
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "nameLastname", "reservation.nameLastname.empty", "");
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "reservation.email.empty", "");
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "showtime", "reservation.showtime.empty", "");
         if (errors.hasErrors()) {
             return;
         }
+        if (validateEmail(reservation.getEmail())) {
+            errors.rejectValue("email", "reservation.email.invalid", "");
 
+        }
+        if (errors.hasErrors()) {
+            return;
+        }
+
+        if (reservation.getNameLastname().split(" ").length < 2) {
+            errors.rejectValue("nameLastname", "reservation.nameLastname.empty", "");
+        }
+        if (errors.hasErrors()) {
+            return;
+        }
+
+        if (reservation.getNameLastname().split(" ")[0].length() < 2) {
+            errors.rejectValue("nameLastname", "reservation.nameLastname.invalid", "");
+        }
+        if (reservation.getNameLastname().split(" ")[1].length() < 2) {
+            errors.rejectValue("nameLastname", "reservation.nameLastname.invalid", "");
+        }
+        if (errors.hasErrors()) {
+            return;
+        }
+        if (reservation.getId() == 0) {
+            int numberOfReservations = reservationService.countByShowtimeId(reservation.getShowtime().getId());
+            System.out.println("NUMBER OF RESERVATIONS IS: " + numberOfReservations);
+            if (numberOfReservations >= reservation.getShowtime().getHall().getCapacity()) {
+                errors.rejectValue("showtime", "reservation.showtime.isFull", "");
+            }
+
+        }
+    }
+
+    boolean validateEmail(String email) {
+        if (!email.contains("@") || !email.contains(".") || email.charAt(0) == '.'
+                || email.charAt(0) == '-' || email.split("@")[0].length() < 2 || email.split("@")[1].length() < 2) {
+            return true;
+        }
+        return false;
     }
 }
